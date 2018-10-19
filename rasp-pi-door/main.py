83eefd6e5f0dcda0c7236aac96766cf5dedc0e91
ark <ark@debian.debian.arksun.me>
2018-10-18 21:21:46 -0400

    10-18

--- rasp-pi-door/main.py
+++ rasp-pi-door/main.py
@@ -7,12 +7,12 @@ def click(x):
     time.sleep(0.2)
     x.off()
 
-p = rfid.rfid()
-while True:
-    p.wait()
-    key_id,name = p.read()
+def callback(uid,name):
     if name == 'Alice':
         click(bsp.sw_module[0])
     if name == 'Bob':
         click(bsp.sw_module[1])
-    time.sleep(1)
+
+p = rfid.rfid()
+while True:
+    p.thread(callback)
--- rasp-pi-door/rfid.py
+++ rasp-pi-door/rfid.py
@@ -18,6 +18,7 @@ class rfid:
         self.rdr = RFID()
         self.util = self.rdr.util()
         self.util.debug = True
+        self.fifo = []
         self.db = {(70,57,222,19,178):'Alice',(70,131,51,20,226):'Bob'}
         signal.signal(signal.SIGINT, self.end_read)
         print("Starting")
@@ -38,14 +39,18 @@ class rfid:
         else:
             print("Error")
             return ((0,0,0,0,0),'error')
-    def run(self):
-        self.th = threading.Thread(target = self.thread)
+    def run(self,callback):
+        self.th = threading.Thread(target = self.thread,args = (callback,))
         self.th.run()
-    def thread(self):
+    def thread(self,callback):
+        self.last = None
         while self.run_flag:
             self.wait()
-            self.read()
-            time.sleep(0.5)
+            res = self.read()
+            if res[0] != self.last[0]:
+                callback(*res)
+                self.last = res
+            time.sleep(0.1)
     def end_read(signal,frame):
         print("Ctrl+C captured, ending read.")
         self.run_flag = False
