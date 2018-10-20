95ccd0460aae7824737eb29f6e5cde80ecccd6bd
ark <arksun9481@gmail.com>
2018-10-20 01:55:39 -0400

    10-20

--- rasp-pi-door/rfid_main.py
+++ rasp-pi-door/rfid_main.py
@@ -11,8 +11,11 @@ import rfid
 import threading
 import os
 import socket
+import ipaddress
 
 p = rfid.rfid()#RFID init
+
+server = ('2607:f2c0:e344:a02::2:3',7000)
 
 #network init
 #-----------------------------------------------------------------#
@@ -20,22 +23,25 @@ os.system('ip -6 addr add 2607:f2c0:e344:a02::2:2/64 dev wlan0')
 s = socket.socket(socket.AF_INET6,socket.SOCK_DGRAM)
 s.setsockopt(socket.SOL_SOCKET,socket.SO_REUSEADDR,1)
 s.bind(('2607:f2c0:e344:a02::2:2',7000))
-s.connect(('2607:f2c0:e344:a02::2:3',7000))
+s.connect(server)
+s.connect(server)
 #-----------------------------------------------------------------#
 def callback(uid):
     global s,p
+    part = uid[0].to_bytes(1,'little')+uid[1].to_bytes(1,'little')+uid[2].to_bytes(1,'little')+uid[3].to_bytes(1,'little')+uid[4].to_bytes(1,'little')
+    addr = b'\x26\x07\xf2\xc0\xe3\x44\x0a\x03\x00\x01\x00'+part
     if uid not in p.db:
-        print('Other:',uid)
+        print('Other:',addr)
         return
     if p.db[uid] == 'Alice':
-        print('Alice',uid)
-        s.send('Alice'.encode())
+        print('Alice',addr)
+        s.send(str(ipaddress.IPv6Address(addr)).encode())
         return
     if p.db[uid] == 'Bob':
-        print('Bob',uid)
-        s.send('Bob'.encode())
+        print('Bob',addr)
+        s.send(str(ipaddress.IPv6Address(addr)).encode())
         return
-    print(p.db[uid],uid)
+    print(p.db[uid],addr)
 
 while True:
     p.thread(callback)
