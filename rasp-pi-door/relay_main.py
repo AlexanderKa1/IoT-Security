b0a607dd6c3984a23e480093e20359028b302b5f
ark <arksun9481@gmail.com>
2018-10-20 02:15:46 -0400

    10-20

--- rasp-pi-door/relay_main.py
+++ rasp-pi-door/relay_main.py
@@ -4,25 +4,36 @@ import threading
 import os
 import socket
 
+def nothing(append=None):
+    pass
+def unlock(append=None):
+    click(bsp.sw_module)
+def on(append=None):
+    bsp.sw_module[0].on()
+    bsp.sw_module[1].on()
+def off(append=None):
+    bsp.sw_module[0].off()
+    bsp.sw_module[1].off()
+
+how = {0:nothing,1:unlock,2:on,3:off}
+
 def click(x):
     x[0].on()
     x[1].on()
     time.sleep(0.2)
     x[0].off()
     x[1].off()
-
+#-----------------------------------------------------------------------
 output = os.popen('ip -6 addr add 2607:f2c0:e344:a02::2:3/64 dev wlan0')
 print(output.read())
 time.sleep(2)
 s = socket.socket(socket.AF_INET6,socket.SOCK_DGRAM)
 s.setsockopt(socket.SOL_SOCKET,socket.SO_REUSEADDR,1)
 s.bind(('2607:f2c0:e344:a02::2:3',7000))
-
+#-----------------------------------------------------------------------
 while True:
     data = s.recv(100)
-    print('recv:',data)
-    if data.decode() == 'Alice':
-        click(bsp.sw_module)
-    if data.decode() == 'Bob':
-        click(bsp.sw_module)
+    pack = eval(data.decode())
+    print(pack)
+    how[pack[0]]()
 
--- rasp-pi-door/rfid_main.py
+++ rasp-pi-door/rfid_main.py
@@ -17,6 +17,8 @@ p = rfid.rfid()#RFID init
 
 server = ('2607:f2c0:e344:a02::2:3',7000)
 
+what = {'scan':1}
+
 #network init
 #-----------------------------------------------------------------#
 os.system('ip -6 addr add 2607:f2c0:e344:a02::2:2/64 dev wlan0')
@@ -30,18 +32,23 @@ def callback(uid):
     global s,p
     part = uid[0].to_bytes(1,'little')+uid[1].to_bytes(1,'little')+uid[2].to_bytes(1,'little')+uid[3].to_bytes(1,'little')+uid[4].to_bytes(1,'little')
     addr = b'\x26\x07\xf2\xc0\xe3\x44\x0a\x03\x00\x01\x00'+part
+    ipv6 = str(ipaddress.IPv6Address(addr))
     if uid not in p.db:
         print('Other:',addr)
         return
+    if p.db[uid] == 'Error':
+        print('Error!')
+        return
     if p.db[uid] == 'Alice':
         print('Alice',addr)
-        s.send(str(ipaddress.IPv6Address(addr)).encode())
+        s.send(str((what['scan'],ipv6)).encode())
         return
     if p.db[uid] == 'Bob':
         print('Bob',addr)
-        s.send(str(ipaddress.IPv6Address(addr)).encode())
+        s.send(str((what['scan'],ipv6)).encode())
         return
     print(p.db[uid],addr)
+    s.send(str((what['scan'],ipv6)).encode())
 
 while True:
     p.thread(callback)
